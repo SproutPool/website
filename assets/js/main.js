82a917c79e50d167f43f934f333ffef3c11efecb
Alessandro Grunwald <agrunwald@convertus.com>
2021-04-23 03:22:03 -0700

    added rewards helper

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 (function($) {
+    
+    // global vars
+    var epochOneStartUTC = 1506635091000; // 2017-10-28 21:44:51 UTC
+    var msPerEpoch = 432000000; // 5 days
 
 	var	$window = $(window),
 		$banner = $('#banner'),
@@ -25,6 +29,8 @@
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
+            var timeoffset = -(new Date().getTimezoneOffset() / 60);
+            $('#timeoffset').html(timeoffset);
 		});
 
 	// Menu.
@@ -41,5 +47,67 @@
 				resetForms: true,
 				side: 'right'
 			});
+
+    // Rewards Helper
+        $('#rewards-helper-btn').click(function() {
+            let inputDate = $("#delegation-date").val();
+            let inputAmount = $("#delegation-amount").val();
+
+            if (inputDate == '') {
+                alert('Please enter a date');
+                return;
+            }
+            if (inputAmount != '') {
+                var rewardsEstimate = (inputAmount*0.055/73).toFixed(6);
+
+                $.get( "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=cardano", function( data ) {
+                    let adaPrice = data[0].current_price;
+
+                    $('#rewards-estimate').html(
+                        rewardsEstimate + " ADA <span style='font-size:80%;'><i>($" + (rewardsEstimate*adaPrice).toFixed(2) + " USD)</i></span>"
+                    );
+                });
+            } else {
+                // calculate rewards and USD value
+                $('#rewards-estimate').html("N/A")
+            }
+
+            let inputTimestampUTC = Date.parse(inputDate);
+
+            // find which epoch this date is in => epoch x
+            let inputsEpoch = getEpochFromTimestamp(inputTimestampUTC);
+
+            let epochPaid = inputsEpoch + 4;
+            let datePaid = timestampToDate(epochOneStartUTC + (inputsEpoch+3) * msPerEpoch);
+            
+            $('#delegation-rewards-timing').html(
+                "<p>At the Beginning of Epoch<br><span style='font-size:150%;'>" + epochPaid + "</span></p><p>On<br><span style='font-size:150%;'>" + datePaid + "</span></p><p>In the Amount* of<br><span id='rewards-estimate' style='font-size:150%;'></span></p><p style='font-size: 85%;'>*estimated rewards amount based on network's average ROS of <b>5.5%</b></p>"
+            );
+        });
+
+        function getEpochFromTimestamp(timestamp) {
+            let timeAfterEpochOne = timestamp - epochOneStartUTC;
+
+            // how many epochs have gone by
+            let passedEpochs = timeAfterEpochOne / msPerEpoch;
+
+            return Math.ceil(passedEpochs);
+        }
+
+        function timestampToDate(timestamp) {
+            var inputDate = new Date(timestamp);
+            let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+            let day = days[inputDate.getDay()];
+            let date = inputDate.getDate();
+            let month = months[inputDate.getMonth()];
+            let year = inputDate.getFullYear();
+            let time = inputDate.getTime();
+
+            let datetime = day + ", " + month + " " + date + ", " + year;
+
+            return datetime;
+        }
 
 })(jQuery);
