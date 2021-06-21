bc420aefb8af25cb89be7b0897ae02863bc12394
Alessandro Grunwald <agrunwald@convertus.com>
2021-06-21 00:56:17 -0700

    dynamic pool stats

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,10 @@
 */
 (function($) {
 
+    // pool155x3l57d4xu52t93f9r55cqaysej4pc2dl3jkc9d6w5ckyu6u7n
+    const poolId = "pool155x3l57d4xu52t93f9r55cqaysej4pc2dl3jkc9d6w5ckyu6u7n";
+    const BLOCKFROST_API_KEY = config.BLOCKFROST_API_KEY_MAINNET;
+
 	var	$window = $(window),
 		$banner = $('#banner'),
 		$body = $('body');
@@ -55,5 +59,48 @@
             }
         });
     });
+
+    // pool info
+    $.ajax({
+        url: "https://cardano-mainnet.blockfrost.io/api/v0/pools/"+poolId,
+        type: "GET",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('project_id', BLOCKFROST_API_KEY);
+        },
+        statusCode: {
+            404: function (response) {
+                alert(response.responseJSON.message);
+            }
+        },
+        success: function(data) {
+            console.log(data);
+            $('#pledge').html(numberWithCommas(data.declared_pledge/1000000));
+            $('#live-stake').html(numberWithCommas(Math.floor(data.live_stake/1000000)));
+            $('#active-stake').html(numberWithCommas(Math.floor(data.active_stake/1000000)));
+            $('#fixed-fee').html(numberWithCommas(data.fixed_cost/1000000));
+            $('#margin-fee').html(data.margin_cost*100);
+            $('#delegators').html(data.live_delegators);
+        }
+    });
+    
+    $.ajax({
+        url: "https://cardano-mainnet.blockfrost.io/api/v0/pools/"+poolId+"/history",
+        type: "GET",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('project_id', BLOCKFROST_API_KEY);
+        },
+        statusCode: {
+            404: function (response) {
+                alert(response.responseJSON.message);
+            }
+        },
+        success: function(data) {
+            console.log(data);
+        }
+    });
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
 })(jQuery);
